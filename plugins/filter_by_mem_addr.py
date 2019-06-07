c8874dd35defaaa90336adf1ea6ae3139a6fe838
Teemu <14867206+teemu-l@users.noreply.github.com>
2019-06-07 15:32:44 +0300

    Add plugin filter_by_mem_addr

---
+++ plugins/filter_by_mem_addr.py
@@ -0,0 +1,40 @@
+"""This plugin demonstrates how to filter the trace by memory range
+Every row which accesses memory between addr and addr+size
+is added to filtered_trace
+"""
+
+from yapsy.IPlugin import IPlugin
+
+class PluginFilterByMemAddress(IPlugin):
+
+    def execute(self, api):
+
+        addr_and_size = api.get_values_from_user(
+            "Filter by memory address",
+            "Give me memory address and size, separated by comma:"
+        )
+
+        if not addr_and_size or len(addr_and_size) != 2:
+            print('Error. Wrong values given')
+            return
+
+        addr, size = addr_and_size
+
+        api.print(f"Filtering by mem access addr from {hex(addr)} to {hex(addr+size)}")
+
+        trace = api.get_visible_trace()
+        filtered_trace = []
+
+        for t in trace:
+            for mem in t['mem']:
+                if addr <= mem['addr'] <= (addr + size):
+                    filtered_trace.append(t.copy())
+                    continue
+
+        trace_len = len(trace)
+        if trace_len > 0:
+            api.print(f"Length of filtered trace: {trace_len}") 
+            api.set_filtered_trace(filtered_trace)
+            api.show_filtered_trace()
+        else:
+            api.print("Filter plugin resulted in empty trace")
